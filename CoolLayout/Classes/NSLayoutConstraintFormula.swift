9b7bf763aede4aac587172d45839dcef36d70573
sudo.park <sudo.park@kakaocorp.com>
2019-10-20 23:01:03 +0900

    update podspec

---
+++ CoolLayout/Classes/NSLayoutConstraintFormula.swift
@@ -0,0 +1,205 @@
+//
+//  NSLayoutConstraintFormulaTest.swift
+//  NSLayoutConstraintFormulaTest
+//
+//  Created by ParkHyunsoo on 2019/10/13.
+//
+
+import UIKit
+
+// TODO: anchor helpers
+
+/**
+ x, y : ==, >=, <= with constatnt
+ d: ==, >=, <= with constant and multiplier
+ */
+
+fileprivate extension NSLayoutConstraint {
+    
+    private var shouldActive: Bool {
+        if let firstView = self.firstItem as? UIView {
+            return firstView.shouldActiveWhenConstraintMade
+        }
+        return false
+    }
+    
+    func activeOrNot() -> NSLayoutConstraint {
+        if shouldActive {
+            self.isActive = true
+        }
+        return self
+    }
+}
+
+public class HalfNSLayoutConstraint<AnchorType: AnyObject> {
+    
+    unowned(unsafe) let referenceAnchor: NSLayoutAnchor<AnchorType>
+    let constant: CGFloat
+    let multiplier: CGFloat
+    let isSystemSpacingMultiplier: Bool
+    
+    init(_ anchor: NSLayoutAnchor<AnchorType>, constant: CGFloat = 0.0,
+         multiplier: CGFloat = 1.0, isSystemSpacingMultiplier: Bool = false) {
+        self.referenceAnchor = anchor
+        self.constant = constant
+        self.multiplier = multiplier
+        self.isSystemSpacingMultiplier = isSystemSpacingMultiplier
+    }
+}
+
+
+public func + <AnchorType>(_ anchor: NSLayoutAnchor<AnchorType>,
+                    _ constant: CGFloat) -> HalfNSLayoutConstraint<AnchorType> {
+    
+    return HalfNSLayoutConstraint(anchor, constant: constant)
+}
+
+public func * <AnchorType>(_ anchor: NSLayoutAnchor<AnchorType>,
+                    _ multiplier: CGFloat) -> HalfNSLayoutConstraint<AnchorType> {
+    
+    return HalfNSLayoutConstraint(anchor, multiplier: multiplier)
+}
+
+@available(iOS 11.0, *)
+public func * (_ anchor: NSLayoutXAxisAnchor,
+               _ multiplier: CGFloat) -> HalfNSLayoutConstraint<NSLayoutXAxisAnchor> {
+    
+    return HalfNSLayoutConstraint(anchor, multiplier: multiplier, isSystemSpacingMultiplier: true)
+}
+
+@available(iOS 11.0, *)
+public func * (_ anchor: NSLayoutYAxisAnchor,
+               _ multiplier: CGFloat) -> HalfNSLayoutConstraint<NSLayoutYAxisAnchor> {
+    
+    return HalfNSLayoutConstraint(anchor, multiplier: multiplier, isSystemSpacingMultiplier: true)
+}
+
+
+public func + (_ half: HalfNSLayoutConstraint<NSLayoutDimension>,
+        _ constatnt: CGFloat) -> HalfNSLayoutConstraint<NSLayoutDimension> {
+    
+    return HalfNSLayoutConstraint(half.referenceAnchor,
+                                  constant: half.constant + constatnt,
+                                  multiplier: half.multiplier)
+}
+
+
+// MARK: - Equal operation
+
+@discardableResult
+public func == <AnchorType>(_ target: NSLayoutAnchor<AnchorType>,
+                     _ half: HalfNSLayoutConstraint<AnchorType>) -> NSLayoutConstraint {
+    
+    if #available(iOS 11.0, *), half.isSystemSpacingMultiplier {
+        if let xAxisTarget = target as? NSLayoutXAxisAnchor,
+            let halfAnchor = half.referenceAnchor as? NSLayoutXAxisAnchor {
+            return xAxisTarget.constraint(equalToSystemSpacingAfter: halfAnchor,
+                                          multiplier: half.multiplier).activeOrNot()
+        }else if let yAxisTarget = target as? NSLayoutYAxisAnchor,
+            let halfAnchor = half.referenceAnchor as? NSLayoutYAxisAnchor {
+            return yAxisTarget.constraint(equalToSystemSpacingBelow: halfAnchor,
+                                          multiplier: half.multiplier)
+                .activeOrNot()
+        }
+    }
+    
+    return target.constraint(equalTo: half.referenceAnchor, constant: half.constant)
+        .activeOrNot()
+}
+
+@discardableResult
+public func == (_ target: NSLayoutDimension,
+         _ half: HalfNSLayoutConstraint<NSLayoutDimension>) -> NSLayoutConstraint {
+    
+    return target
+        .constraint(equalTo: half.referenceAnchor as! NSLayoutDimension,
+                    multiplier: half.multiplier,
+                    constant: half.constant)
+        .activeOrNot()
+}
+
+@discardableResult
+public func == (_ target: NSLayoutDimension, _ constant: CGFloat) -> NSLayoutConstraint {
+    return target.constraint(equalToConstant: constant)
+        .activeOrNot()
+}
+
+
+
+// MARK: - Less than ro Equal
+
+@discardableResult
+public func <= <AnchorType> (_ target: NSLayoutAnchor<AnchorType>,
+                      _ half: HalfNSLayoutConstraint<AnchorType>) -> NSLayoutConstraint {
+    
+    if #available(iOS 11.0, *), half.isSystemSpacingMultiplier {
+        if let xAxisTarget = target as? NSLayoutXAxisAnchor,
+            let halfAnchor = half.referenceAnchor as? NSLayoutXAxisAnchor {
+            return xAxisTarget.constraint(lessThanOrEqualToSystemSpacingAfter: halfAnchor,
+                                          multiplier: half.multiplier)
+                .activeOrNot()
+        }else if let yAxisTarget = target as? NSLayoutYAxisAnchor,
+            let halfAnchor = half.referenceAnchor as? NSLayoutYAxisAnchor {
+            return yAxisTarget.constraint(lessThanOrEqualToSystemSpacingBelow: halfAnchor,
+                                          multiplier: half.multiplier)
+                .activeOrNot()
+        }
+    }
+    
+    return target.constraint(lessThanOrEqualTo: half.referenceAnchor,
+                             constant: half.constant)
+        .activeOrNot()
+}
+
+@discardableResult
+public func <= (_ target: NSLayoutDimension,
+         _ half: HalfNSLayoutConstraint<NSLayoutDimension>) -> NSLayoutConstraint {
+    
+    return target
+        .constraint(lessThanOrEqualTo: half.referenceAnchor as! NSLayoutDimension,
+                    multiplier: half.multiplier,
+                    constant: half.constant)
+        .activeOrNot()
+}
+
+
+
+
+// MARK: - Greater than or Equal
+
+@discardableResult
+public func >= <AnchorType> (_ target: NSLayoutAnchor<AnchorType>,
+                      _ half: HalfNSLayoutConstraint<AnchorType>) -> NSLayoutConstraint {
+    
+    if #available(iOS 11.0, *), half.isSystemSpacingMultiplier {
+        if let xAxisTarget = target as? NSLayoutXAxisAnchor,
+            let halfAnchor = half.referenceAnchor as? NSLayoutXAxisAnchor {
+            return xAxisTarget.constraint(greaterThanOrEqualToSystemSpacingAfter: halfAnchor,
+                                          multiplier: half.multiplier)
+                .activeOrNot()
+        }else if let yAxisTarget = target as? NSLayoutYAxisAnchor,
+            let halfAnchor = half.referenceAnchor as? NSLayoutYAxisAnchor {
+            return yAxisTarget.constraint(greaterThanOrEqualToSystemSpacingBelow: halfAnchor,
+                                          multiplier: half.multiplier)
+                .activeOrNot()
+        }
+    }
+    
+    return target
+        .constraint(greaterThanOrEqualTo: half.referenceAnchor,
+                    constant: half.constant)
+        .activeOrNot()
+}
+
+
+@discardableResult
+public func >= (_ target: NSLayoutDimension,
+         _ half: HalfNSLayoutConstraint<NSLayoutDimension>) -> NSLayoutConstraint {
+    
+    return target
+        .constraint(greaterThanOrEqualTo: half.referenceAnchor as! NSLayoutDimension,
+                    multiplier: half.multiplier,
+                    constant: half.constant)
+        .activeOrNot()
+}
+
---
+++ CoolLayout/Classes/UIView+Extension.swift
@@ -0,0 +1,117 @@
+//
+//  UIView+Extension.swift
+//  CoolLayout
+//
+//  Created by sudo.park on 19/10/2019.
+//
+
+import UIKit
+
+// MARK: extension for < swift 5.1
+
+extension NSCoding where Self: UIView {
+
+    public static var autoLayout: Self {
+        let view = self.init()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }
+    
+    public init(autoLayout: (Self) -> Void) {
+        self.init()
+        self.translatesAutoresizingMaskIntoConstraints = false
+        autoLayout(self)
+    }
+    
+    public init(autoLayout: ((Self) -> Void)...) {
+        self.init()
+        autoLayout.forEach {
+            $0(self)
+        }
+    }
+    
+    public init(autoLayout: [((Self) -> Void)]) {
+        self.init()
+        autoLayout.forEach {
+            $0(self)
+        }
+    }
+    
+    public static func lazy(_ initFunction: () -> Self) -> Self {
+        return initFunction()
+    }
+
+}
+
+
+// MARK: - apply decorations
+
+extension NSCoding where Self: UIView {
+    
+    @discardableResult
+    public func apply(_ decorating: (Self) -> Void) -> Self {
+        decorating(self)
+        return self
+    }
+    
+    @discardableResult
+    public func apply(_ decoratings: ((Self) -> Void)...) -> Self {
+        decoratings.forEach {
+            $0(self)
+        }
+        return self
+    }
+
+    @discardableResult
+    public func apply(_ decoratings: [(Self) -> Void]) -> Self {
+        decoratings.forEach {
+            $0(self)
+        }
+        return self
+    }
+    
+    @discardableResult
+    public func apply(_ decorating: (UIView) -> Void) -> Self {
+        decorating(self)
+        return self
+    }
+    
+    @discardableResult
+    public func apply(_ decoratings: ((UIView) -> Void)...) -> Self {
+        decoratings.forEach {
+            $0(self)
+        }
+        return self
+    }
+
+    @discardableResult
+    public func apply(_ decoratings: [(UIView) -> Void]) -> Self {
+        decoratings.forEach {
+            $0(self)
+        }
+        return self
+    }
+}
+
+
+// MARK: - extension for active constraints
+
+extension UIView {
+    
+    fileprivate struct FlagHolder {
+        static var flagMap: [Int: Bool] = [:]
+    }
+    
+    var shouldActiveWhenConstraintMade: Bool {
+        let key = self.hash
+        return FlagHolder.flagMap[key] ?? false
+    }
+    
+    
+    public func activate(_ buildConstraint: (UIView) -> Void) {
+        let key = self.hash
+        FlagHolder.flagMap[key] = true
+        buildConstraint(self)
+        FlagHolder.flagMap[key] = false
+    }
+}
