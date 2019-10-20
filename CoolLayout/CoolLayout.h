03f959b53abbd3b3f2aafbc52f8db3fb85200038
sudo.park <sudo.park@kakaocorp.com>
2019-10-20 16:16:33 +0900

    add carthage

---
+++ CoolLayout/CoolLayout.h
@@ -0,0 +1,19 @@
+//
+//  CoolLayout.h
+//  CoolLayout
+//
+//  Created by ParkHyunsoo on 2019/10/20.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoolLayout.
+FOUNDATION_EXPORT double CoolLayoutVersionNumber;
+
+//! Project version string for CoolLayout.
+FOUNDATION_EXPORT const unsigned char CoolLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoolLayout/PublicHeader.h>
+
+
